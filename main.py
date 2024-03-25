0ef7494c6755f573ebc9718e3ff96ff2ac5be934
stasyyellow <anastasia.nazinyan9@gmail.com>
2024-03-25 23:22:28 +0400

    icon

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption('Игра ТИР')
-icon = pygame.image.load('img/.jpg')
+icon = pygame.image.load('img/тир_icon.jpg')
 pygame.display.set_icon(icon)
 
 
