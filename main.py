407de4864eb0db61b39ec7891f6d91ce6b243059
stasyyellow <anastasia.nazinyan9@gmail.com>
2024-03-25 23:01:28 +0400

    переменные с конст. значениями и окошко

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import pygame
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption('Игра ТИР')
+icon = pygame.image.load('img/.jpg')
+pygame.display.set_icon(icon)
+
+
 running = True
 while running:
     pass
